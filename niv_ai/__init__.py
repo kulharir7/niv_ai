122b945f3a727e8eadd5b5da3f4c84336071d3a7
kulharir7 <kulharir7@gmail.com>
2026-02-20 11:03:29 +0530

    feat: add Sentry error tracking

--- niv_ai/__init__.py
+++ niv_ai/__init__.py
@@ -1,1 +1,13 @@
 __version__ = "1.0.1"
+
+try:
+    import sentry_sdk
+
+    sentry_sdk.init(
+        dsn="https://abc2a6d826b9fbb892612a13d7315ca5@o4510916592795648.ingest.de.sentry.io/4510916601774160",
+        traces_sample_rate=0.1,
+        send_default_pii=True,
+        default_integrations=False,
+    )
+except Exception:
+    pass
